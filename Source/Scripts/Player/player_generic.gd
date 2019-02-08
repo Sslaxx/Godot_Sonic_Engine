27ada468f85f76cea50d5d8e882dcdf173ea6a40
Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>
2019-02-08 12:57:06 +0000

    Floor slope handling, pivots and sanity checks.
    Starting work on how floor slopes and checks are handled. Plus sanity
    checking for the speed helper functions!
    
    Signed-off-by: Stuart "Sslaxx" Moore <stuart@sslaxx.co.uk>

--- Source/Scripts/Player/player_generic.gd
+++ Source/Scripts/Player/player_generic.gd
@@ -145,7 +145,7 @@ func _physics_process (delta):
 		movement_state_machine_air (delta)		# Being in the air.
 	velocity.x = (player_speed * movement_direction)	# Work out velocity from speed * direction.
 	if (is_on_floor ()):								# Make sure gravity applies.
-		velocity.y = (0 if velocity.y > 0.0 else velocity.y)
+		velocity.y = (0 if velocity.y > 0.0 else (0 if velocity.y > -32 else velocity.y))
 		floor_snap = Vector2 (0, 32)
 	else:
 		velocity.y += (player_gravity/15)
@@ -182,9 +182,11 @@ func change_anim (anim_to_change_to):
    IMPORTANT: Player movement in a cutscene has to be handled directly by any scene(s) running the cutscene.
 """
 func get_acceleration_mult ():
-	var acceleration_mult = 1.0	# Every factor gets added to/taken away from this value.
-	if (moving_in == "nil"):	# Not moving, so there's no acceleration. KEEP THIS LAST.
-		acceleration_mult = 0.0	# This MUST override any other calculations to acceleration rate.
+	var acceleration_mult = 1.0		# Every factor gets added to/taken away from this value.
+	if (!is_on_floor ()):			# If not on the floor, emulate "air friction".
+		acceleration_mult -= 0.85
+	if (moving_in == "nil" || acceleration_mult < 0):		# If not moving, or acceleration_mult is not sane, zero it.
+		acceleration_mult = 0.0		# This MUST override any other calculations to acceleration rate.
 	return (acceleration_mult)
 
 """
@@ -196,6 +198,7 @@ func get_acceleration_mult ():
 """
 func get_deceleration_mult ():
 	var deceleration_mult = 1.0	# Every factor gets added to/taken away from this value.
+	deceleration_mult = (0 if deceleration_mult < 0 else deceleration_mult)		# Keep deceleration rate sane.
 	if (player_movement_state == MovementState.STATE_CUTSCENE):	# In a cutscene, so the multiplier is 4. KEEP THIS LAST.
 		deceleration_mult = 4.0	# This MUST override any other calculations to deceleration rate.
 	return (deceleration_mult)
@@ -207,6 +210,7 @@ func get_deceleration_mult ():
 """
 func get_max_player_speed_mult ():
 	var max_speed_mult = 1.0	# Every factor gets added to/taken away from this value.
+	max_speed_mult = (0 if max_speed_mult < 0 else max_speed_mult)		# Sanity checking.
 	return (max_speed_mult)
 
 """
@@ -273,6 +277,10 @@ func movement_state_machine_speed (delta):
 func movement_state_machine_ground (delta):
 	if (player_movement_state & MovementState.STATE_JUMPING):	# Finished jumping? Turn off the jump state.
 		player_movement_state &= ~MovementState.STATE_JUMPING
+	if (!$PlayerPivot.enabled):	# If on the ground and the pivot/floor edge detectors are not enabled, enable them.
+		$PlayerPivot.enabled = true
+		$FloorEdgeLeft.enabled = true
+		$FloorEdgeRight.enabled = true
 	# Change the currently playing animation based on the player's current speed...
 	if (player_speed > 0):
 		if (player_speed < walk_limit):	# ...walking...
@@ -295,6 +303,10 @@ func movement_state_machine_ground (delta):
    Does state machine checks while the player is in the air, either jumping or falling.
 """
 func movement_state_machine_air (delta):
+	if ($PlayerPivot.enabled):	# If in the air and the pivot/floor edge detectors are enabled, disable them.
+		$PlayerPivot.enabled = false
+		$FloorEdgeLeft.enabled = false
+		$FloorEdgeRight.enabled = false
 	if (is_on_wall ()):		# Against a wall? Not on the ground? Then negate running speed.
 		player_speed = 0
 	# Change the currently playing animation based on the player's current speed...
